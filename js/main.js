d71456ea6239765b08933abc7fb2a9f384f6faa9
lorossi <lo.rossi@live.com>
2020-10-13 18:22:21 +0200

    updated stuff

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 let check_loaded;
+let started;
 
 $(document).ready(function () {
   check_loaded = setInterval(check, 100);
+  started = false;
 
   $(".okbutton").click(function () {
     $(".container").hide();
+    $(".instructions").css({
+      "display": "inline-block"
+    });
+    started = true;
   });
+
+  $("#sketch").click(function () {
+    if (!started) return;
+      $(".instructions").delay(5000).fadeOut(3000);
+    })
 });
 
 function check() {
