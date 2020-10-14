07fc60347dff8e09ac61198dac1f28c987cee90e
lorossi <lo.rossi@live.com>
2020-10-14 20:54:53 +0200

    added flying dragons and now santas spit fire

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,8 @@
-let santas_number;
+let santas_number, dragons_number, fires_number;
 let img_number = 5;
 let santas = [];
+let dragons = [];
+let fires = [];
 let img_palette = [];
 let background_palette; // red and green
 
@@ -29,8 +31,12 @@ function setup() {
 
   if (windowWidth > 600) {
     santas_number = 50;
+    dragons_number = 35;
+    fires_number = 25;
   } else {
     santas_number = 20;
+    dragons_number = 15;
+    fires_number = 10;
   }
 
   for (let i = 0; i < santas_number; i++) {
@@ -41,6 +47,20 @@ function setup() {
 
     santas.push(
       new Santa(sx, sy, simg)
+    )
+  }
+
+  for (let i = 0; i < dragons_number; i++) {
+    let dx, dy, dimg;
+    dx = random(width);
+    dy = random(height);
+    dimg = {
+      name: "assets/dragon",
+      extension: "png"
+    };
+
+    dragons.push(
+      new Dragon(dx, dy, dimg)
     )
   }
 
@@ -60,6 +80,24 @@ function draw() {
     s.show();
     s.move();
   });
+
+  dragons.forEach((d, i) => {
+    d.show();
+    d.move();
+  });
+
+  fires.forEach((f, i) => {
+    f.show();
+    f.move();
+  });
+
+  for (let i = fires.length - 1; i >= 0; i--) {
+    if (!fires[i].alive) {
+      fires.splice(i, 1);
+    }
+  }
+
+
 }
 
 class Santa {
@@ -124,8 +162,25 @@ class Santa {
       this.position.add(this.velocity);
     }
 
-    if (this.position.x - this.image.width / 2 * this.scl > width) this.position.x -= width;
-    else if (this.position.x + this.image.width / 2 * this.scl < 0) this.position.x += width;
+    if (this.position.x - this.image.width * this.scl > width) this.position.x -= width;
+    else if (this.position.x + this.image.width * this.scl < 0) this.position.x += width;
+
+    if (fires.length < fires_number && random(1) < .05 && mouse_pressed) {
+      let fx, fy, fvel, fdir, fimg, fscl;
+      fx = this.position.x,
+      fy = this.position.y;
+      fvel = this.speed * 5;
+      fdir = this.velocity.heading();
+      fimg = {
+        name: "assets/fire",
+        extension: "png"
+      };
+      fscl = this.scl / 2;
+
+      fires.push(
+        new Fire(fx, fy, fvel, fdir, fimg, fscl)
+      );
+    }
   }
 
   reset() {
@@ -134,6 +189,86 @@ class Santa {
   }
 }
 
+class Fire {
+  constructor(x, y, velocity, direction, image_path, scl) {
+    this.position = createVector(x, y);
+    this.image = loadImage(`${image_path.name}.${image_path.extension}`);
+    this.inverted_image = loadImage(`${image_path.name}-inverted.${image_path.extension}`);
+
+    this.scl = scl;
+    this.distance = random(10);
+    this.direction = direction;
+    this.velocity = createVector(velocity, 0).rotate(direction);
+
+    this.alive = true;
+  }
+
+  show() {
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(this.direction + PI/2);
+    imageMode(CENTER);
+
+    if (mouse_pressed) {
+      image(this.inverted_image, 0, 0, this.inverted_image.width * this.scl, this.inverted_image.height * this.scl);
+    } else {
+      image(this.image, 0, 0, this.image.width * this.scl, this.image.height * this.scl);
+    }
+    pop();
+  }
+
+  move() {
+    this.position.add(this.velocity);
+    if (this.position.x - this.image.width * this.scl > width) this.alive = false;
+    else if (this.position.x + this.image.width * this.scl < 0) this.alive = false;
+
+    if (this.position.y - this.image.height * this.scl > height) this.alive = false;
+    else if (this.position.y + this.image.height * this.scl < 0) this.alive = false;
+  }
+}
+
+
+class Dragon {
+  constructor(x, y, image_path) {
+    this.position = createVector(x, y);
+    this.image = loadImage(`${image_path.name}.${image_path.extension}`);
+    this.inverted_image = loadImage(`${image_path.name}-inverted.${image_path.extension}`);
+
+    this.distance = random(10);
+    this.period = random(0.25, 1) * 60;
+    this.phi = random(1000);
+
+    if (windowWidth > 600) {
+      this.scl = map(this.distance, 0, 10, 1, 0.25);
+      this.speed = map(this.distance, 0, 10, 15, 4);
+    } else {
+      this.scl = map(this.distance, 0, 10, 0.75, 0.2);
+      this.speed = map(this.distance, 0, 10, 10, 2);
+    }
+
+    this.velocity = createVector(this.speed, 0).rotate(this.phi);
+  }
+
+  show() {
+    if (!mouse_pressed) return
+    push();
+    translate(this.position.x, this.position.y);
+    rotate(this.phi + frameCount * TWO_PI / this.period);
+    imageMode(CENTER);
+    image(this.inverted_image, 0, 0, this.inverted_image.width * this.scl, this.inverted_image.height * this.scl);
+    pop();
+  }
+
+  move() {
+    if (!mouse_pressed) return
+    this.position.add(this.velocity);
+    if (this.position.x - this.image.width * this.scl > width) this.position.x -= width;
+    else if (this.position.x + this.image.width * this.scl < 0) this.position.x += width;
+
+    if (this.position.y - this.image.height * this.scl > height) this.position.y -= height;
+    else if (this.position.y + this.image.height * this.scl < 0) this.position.y += height;
+  }
+}
 
 function mousePressed() {
   if (!sound_loaded) return;
